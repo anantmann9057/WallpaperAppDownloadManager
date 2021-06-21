f8165c23e829564ffd8a4cf7e4c16d1c155ff89a
anantmann9057 <anantmann9057@gmail.com>
2021-06-21 11:06:16 +0530

    added search fragment animation

--- app/src/main/java/com/example/downloadcoroutines/ui/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/downloadcoroutines/ui/fragments/SearchFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.downloadcoroutines.R
+import kotlinx.android.synthetic.main.fragment_search.*
 
 
 class SearchFragment : Fragment() {
@@ -15,11 +16,17 @@ class SearchFragment : Fragment() {
 
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        animeSearch.setAnimationFromUrl("https://assets10.lottiefiles.com/packages/lf20_gd2nnpqy.json")
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_search, container, false)
     }
 
